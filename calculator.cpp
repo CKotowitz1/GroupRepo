3bc7e11bb31bd68af77d867acc59bd82048929dd
Cayden <ckotowitz1@saddleback.edu>
2022-10-10 20:51:42 -0700

    Calculator Base code

---
+++ calculator.cpp
@@ -0,0 +1,6 @@
+#include "calculator.h"
+
+int add(int first, int second)
+{
+	return first + second;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Is this working?" << endl;
+	
+	return 0;
+}
